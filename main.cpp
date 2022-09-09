f866bcb759a5db5f50a2eff1cfeea11ecccbc483
Kevin Buffardi <kbuffardi@csuchico.edu>
2022-09-08 20:48:40 -0700

    Add 4 code snippets

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 // Problem 1
-int score;
+int score = 90;
 cout << "Score? ";
 cin >> score;
 score = score + (score * 0.1);
@@ -7,8 +7,21 @@ cout << "Updated: " << score << endl;
 
 // Problem 2
 double feet = 6.02;
-
-if( feet / 3 ){
-    cout
+if( feet / 3 > 1 ){
+    cout << feet << " is at least " << (int) feet / 3 << " yards";
 }
 
+// Problem 3
+char letter = 'D';
+if( letter >= 'A' & letter <= 'Z' ){
+    cout << "Upper";
+}
+
+// Problem 4
+int value = 50;
+
+if( value > 0 ){
+    int value = 100;
+}
+
+cout << "Result is " << value << endl;
