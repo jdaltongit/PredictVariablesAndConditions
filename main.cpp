129599733c1d620b306c18b6f5c935c7d3df53eb
Kevin Buffardi <kbuffardi@csuchico.edu>
2022-09-08 20:48:15 -0700

    Add 4 code snippets

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Problem 1
+int score;
+cout << "Score? ";
+cin >> score;
+score = score + (score * 0.1);
+cout << "Updated: " << score << endl;
+
+// Problem 2
+double feet = 6.02;
+
+if( feet / 3 ){
+    cout
+}
+
